ed1dc49b3c96fccfd262032f97f9d2cf40f8edb0
goldjin06 <goldjin06@naver.com>
2025-04-26 13:48:55 +0900

    여기까지

--- babData.py
+++ babData.py
@@ -1,12 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
- 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://mportal.cau.ac.kr/main.do',headers=headers)
- 
-soup = BeautifulSoup(data.text, 'html.parser')
-s = soup.select('#carteP005 > li > dl > dd > label > div')
-f = open("./새파일.txt", 'w')
-f.write(str(soup))
-f.close()
-print(soup)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import bs4
+driver = webdriver.Edge()
+
+driver.get('https://mportal.cau.ac.kr/main.do')
+driver.set_window_size(400,1000)
+
+
+
+req = driver.page_source
+soup = bs4.BeautifulSoup(req, "html.parser")
+
+contents = soup.select("#carteP005 > li > dl > dd > label > .nb-p-04-detail")
+print(contents[0].getText(), contents[1].getText())
+
+
+def breakfast():
+    link = driver.find_element('#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child(1)')
+    link.click()
+    time.sleep(5)
+    contents = soup.select("#carteP005 > li > dl > dd > label > .nb-p-04-detail")
+    return(contents)
+
+
+
+print(breakfast())
+
+driver.quit()
