87518c362d9e795170faa526d5c2ffd7f556949b
goldjin06 <goldjin06@naver.com>
2025-04-27 02:16:04 +0900

    당일 급식 크롤링 완료

--- babData.py
+++ babData.py
@@ -2,29 +2,60 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import bs4
-driver = webdriver.Edge()
-
-driver.get('https://mportal.cau.ac.kr/main.do')
-driver.set_window_size(400,1000)
 
 
 
-req = driver.page_source
-soup = bs4.BeautifulSoup(req, "html.parser")
-
-contents = soup.select("#carteP005 > li > dl > dd > label > .nb-p-04-detail")
-print(contents[0].getText(), contents[1].getText())
-
-
-def breakfast():
-    link = driver.find_element('#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child(1)')
-    link.click()
-    time.sleep(5)
+def getMenu(driver):
+    soup = bs4.BeautifulSoup(driver.page_source, "html.parser")
     contents = soup.select("#carteP005 > li > dl > dd > label > .nb-p-04-detail")
-    return(contents)
+    
+    allMenu = []
+    
+    for content in contents:
+
+        contentList = content.select("div > div > p > .ng-binding")
+        if contentList[2].getText() == "0 원":
+            continue
+
+        menuInfo = {"place" : contentList[0].getText(),
+                    "time" : contentList[1].getText()}
+
+        menuList = content.select("div > p")
+        menu = []
+        for i in range(3,len(menuList)):
+            menu.append(menuList[i].getText())
+
+        menuInfo["menu"] = menu
+        allMenu.append(menuInfo)
 
 
+    return(allMenu)
 
-print(breakfast())
 
-driver.quit()
+def breakfast(driver):
+    link = driver.find_element(By.CSS_SELECTOR,'#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child(1)')
+    link.click()
+    time.sleep(0.1)
+    return(getMenu(driver))
+
+def lunch(driver):
+    link = driver.find_element(By.CSS_SELECTOR,'#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child(2)')
+    link.click()
+    time.sleep(0.1)
+    return(getMenu(driver))
+
+def dinner(driver):
+    link = driver.find_element(By.CSS_SELECTOR,'#P005 > div > div > div > div > ol > li > header > div.nb-right.nb-t-right > ol > li:nth-child(3)')
+    link.click()
+    time.sleep(0.1)
+    return(getMenu(driver))
+
+if __name__ == "__main__":
+    driver = webdriver.Edge()
+    driver.set_window_size(400,1000) # 반응형웹이라서 창 크기
+    driver.get('https://mportal.cau.ac.kr/main.do')
+    print(breakfast(driver))
+    print(lunch(driver))
+    print(dinner(driver))
+
+    driver.quit()
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import discord
 from dotenv import load_dotenv
 import os
+import babData as bab
+from selenium import webdriver
+
+
+driver = webdriver.Edge()
+driver.set_window_size(400,1000) # 반응형웹이라서 창 크기
+driver.get('https://mportal.cau.ac.kr/main.do')
+a = [bab.breakfast(driver), bab.lunch(driver), bab.dinner(driver)]
 
 load_dotenv()
 bot = discord.Bot()
@@ -11,7 +19,7 @@ async def on_ready():
 
 @bot.slash_command(name="hello", description="Say hello to the bot")
 async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond("hey!")
+    await ctx.respond(f'''{a}''')
 
 
 bot.run(os.getenv("TOKEN"))
